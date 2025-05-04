0906acb0d30435399fe609246930d12128b897dd
emmabateman <emmalbateman@gmail.com>
2025-05-04 15:12:15 -0700

    initial landing page (under construction)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return(<p>Hello world</p>);
+  return(
+    <div className="container text-center">
+      <div className="row">
+        <h1>Emma's website</h1>
+      </div>
+      <div className="row">
+        <h2>Welcome to My Website</h2>
+      </div>
+      <div className="row">
+        <h3>This website is under construction</h3>
+      </div>
+    </div>
+  );
 }
