8631f3a901db99920d2058dae0628bbed4d7ffba
emmabateman <emmalbateman@gmail.com>
2025-05-14 15:12:39 -0700

    clue generation

--- app/mathdoku/components/square.tsx
+++ app/mathdoku/components/square.tsx
@@ -1,10 +1,15 @@
 import styles from '../styles.module.css';
 
-function Square({ value, selected, handleClick }:
-		{ value: number, selected: boolean }) {
+function Square({ value, clueText, clueIndices, selected, borderStyles }:
+		{ value: number,
+		  clueText: string,
+		  clueIndices: [number, number][],
+		  selected: boolean,
+		  borderStyles: string[]}) {
   return (
-    <div className={styles.square}>
+    <div className={styles.square} style={{ borderColor: borderStyles.join(" ")}}>
       <div className={`${styles.highlight} ${selected ? styles.active : ""}`}>
+        <p className="position-absolute">{clueText}</p>
         <h1>{value}</h1>
       </div>
     </div>
