8a231712ae01e58f99b291802c34ab49ee2e8acb
emmabateman <emmalbateman@gmail.com>
2025-05-14 15:12:39 -0700

    clue generation

--- app/mathdoku/generate_puzzle.ts
+++ app/mathdoku/generate_puzzle.ts
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
+const numClues = 30;
+const maxClueSize = 6;
+
 interface Clue {
   indices: [number, number][];
   operation?: "+|-|/|*";
@@ -83,6 +86,85 @@ function generateClues(size: number, solution: number[][]): Clue[] {
       clues.push({ indices: [[i, j]], value: solution[i][j] });
     }
   }
+
+  //merge clues
+  while (clues.length > numClues) {
+    let clue1 = clues.splice(Math.floor(Math.random() * clues.length), 1)[0];
+    let clue2 = clues.splice(Math.floor(Math.random() * clues.length), 1)[0];
+
+    if (clue1.indices.length + clue2.indices.length > maxClueSize) {
+      //merged clue would be too big, put back
+      clues.push(clue1);
+      clues.push(clue2);
+      continue;
+    }
+
+    let areAdjacent = false;
+    for (let idx1 of clue1.indices) {
+      for (let idx2 of clue2.indices) {
+	//check if squares are neighboring horizontally
+        if (idx1[0] == idx2[0] && Math.abs(idx1[1] - idx2[1]) == 1) {
+          areAdjacent = true;
+	}
+	//check if squares are neighboring vertically
+	if (idx1[1] == idx2[1] && Math.abs(idx1[0] - idx2[0]) == 1) {
+	  areAdjacent = true;
+	}
+      }
+    }
+
+    if (!areAdjacent) {
+      //can't merge non-adjacent clues, put back
+      clues.push(clue1);
+      clues.push(clue2);
+      continue;
+    }
+
+    let mergedClue : Clue = {
+      indices: clue1.indices.concat(clue2.indices).sort((a,b) => {
+        if (a[0] != b[0]) {
+	  return a[0] - b[0];
+	}
+	return a[1] - b[1];
+      }),
+      value: 0
+    }
+
+    let possibleOperations = ["+", "*"]; //any clue can have commutative operations
+    if (mergedClue.indices.length == 2) {
+      possibleOperations.push("-");
+
+      if (Math.max(clue1.value, clue2.value) %
+	  Math.min(clue1.value, clue2.value) == 0) {
+	// one value divides into the other
+	possibleOperations.push("/");
+      }
+    }
+
+    mergedClue.operation = possibleOperations[
+	    Math.floor(Math.random() * possibleOperations.length)];
+    switch(mergedClue.operation) {
+      case "+":
+        mergedClue.value = mergedClue.indices.map(([x, y]) => solution[x][y])
+                                             .reduce((a, b) => a + b);
+	break;
+      case "*":
+        mergedClue.value = mergedClue.indices.map(([x, y]) => solution[x][y])
+                                             .reduce((a, b) => a * b);
+        break;
+      case "-":
+	mergedClue.value = Math.max(clue1.value, clue2.value) -
+			   Math.min(clue1.value, clue2.value);
+        break;
+      case "/":
+        mergedClue.value = Math.max(clue1.value, clue2.value) /
+			   Math.min(clue1.value, clue2.value);
+        break;
+    }
+
+    clues.push(mergedClue);
+  }
+
   return clues;
 }
 
