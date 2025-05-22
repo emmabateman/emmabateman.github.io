a422136bf3a9cad76f0db57e1544c252bf0141fd
Emma Bateman <emmalbateman@gmail.com>
2025-05-21 23:59:16 -0700

    formatting fixes

--- app/mathdoku/components/controls.tsx
+++ app/mathdoku/components/controls.tsx
@@ -1,5 +1,5 @@
 interface Config {
-  gridSize: number;
+  size: number;
   autoSolve: boolean;
 }
 
@@ -7,17 +7,19 @@ function Controls({ config, numberPress }:
 		  { config: Config,
                     numberPress: (n: number) => void}) {
   return (
-    <div class="mt-4 flex-row">
+    <div className="mt-4 flex-row">
       {[...new Array(config.size).keys()].map((n) =>
         <button className="btn btn-primary btn-lg m-1" onClick={() => { numberPress(n + 1) }} key={n}>
           {n + 1}
 	</button>
       )}
       <button className="btn btn-primary btn-lg" onClick={() => { numberPress(0) }}>
-          <i class="bi bi-x-circle"/>
+          <i className="bi bi-x-circle"/>
       </button>
     </div>
   );
 }
 
-export { Config, Controls };
+export { Controls };
+
+export type { Config };
--- app/mathdoku/page.tsx
+++ app/mathdoku/page.tsx
@@ -1,22 +1,26 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { Grid } from './components/grid';
-import { Config, Controls } from './components/controls';
-import { generatePuzzle, Puzzle } from './generate_puzzle';
+import { Grid } from "./components/grid";
+import { Config, Controls } from "./components/controls";
+import { generatePuzzle, Puzzle } from "./generate_puzzle";
 
 export default function Page() {
   const [config, setConfig] = useState<Config>({ autoSolve: false, size: 9 });
 
   const [possibleValues, setPossibleValues] = useState<number[][][]>(
-    new Array(config.size).fill(null).map(() => new Array(config.size).fill(
-      [...Array(config.size).keys()].map(n => n + 1) //numbers 1 through size
-    ))
+    new Array(config.size).fill(null).map(() =>
+      new Array(config.size).fill(
+        [...Array(config.size).keys()].map((n) => n + 1) //numbers 1 through size
+      )
+    )
   );
 
   const [finalValues, setFinalValues] = useState<number[][]>(
-    new Array(config.size).fill(null).map(() => new Array(config.size).fill(null))
+    new Array(config.size)
+      .fill(null)
+      .map(() => new Array(config.size).fill(null))
   );
 
   const [activeSquareId, setActiveSquareId] = useState<string>("");
@@ -37,7 +41,8 @@ export default function Page() {
       if (n >= 1 && n <= config.size) {
         newFinalValues[x][y] = n;
       }
-      if (n == 0) { //clear square
+      if (n == 0) {
+        //clear square
         newFinalValues[x][y] = null;
       }
       setFinalValues(newFinalValues);
@@ -45,17 +50,25 @@ export default function Page() {
   }
 
   return (
-    <div tabIndex="0" onKeyDown={(e) => {
-	    if (e.key.match(/[1-9]/g)) {
-		    numberPress(e.key)
-	    }
-        }}>
+    <div
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key.match(/[1-9]/g)) {
+          numberPress(parseInt(e.key));
+        }
+      }}
+    >
       <h1>Mathdoku</h1>
       <h3 className="mb-4">My take on a KenKen clone</h3>
-      <Grid config={config} possibleValues={possibleValues}
-            finalValues={finalValues} clues={puzzle ? puzzle.clues : []}
-	    activeSquareId={activeSquareId} setActiveSquareId={setActiveSquareId}/>
-      <Controls config={config} numberPress={numberPress}/>
+      <Grid
+        config={config}
+        possibleValues={possibleValues}
+        finalValues={finalValues}
+        clues={puzzle ? puzzle.clues : []}
+        activeSquareId={activeSquareId}
+        setActiveSquareId={setActiveSquareId}
+      />
+      <Controls config={config} numberPress={numberPress} />
     </div>
   );
 }
