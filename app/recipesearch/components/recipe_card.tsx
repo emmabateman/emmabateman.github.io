ed989552f909c456f94881dd7106e144205b0be3
Emma Bateman <emmalbateman@gmail.com>
2025-05-24 23:58:00 -0700

    show and hide instructions on recipe card

--- app/recipesearch/components/recipe_card.tsx
+++ app/recipesearch/components/recipe_card.tsx
@@ -1,51 +1,96 @@
-import { Recipe, Ingredient } from './recipe'
+"use client";
 
-function RecipeCard({ recipe, myIngredients, addToShoppingList }:
-		    { recipe: Recipe, myIngredients: JSON[],
-			    addToShoppingList: (items: string[]) => void }) {
-  function numMatchingIngredients() : number {
+import { useState } from "react";
+
+import { Recipe, Ingredient } from "./recipe";
+
+function RecipeCard({
+  recipe,
+  myIngredients,
+  addToShoppingList,
+}: {
+  recipe: Recipe;
+  myIngredients: JSON[];
+  addToShoppingList: (items: string[]) => void;
+}) {
+  const [showInstructions, setShowInstructions] = useState<boolean>(false);
+
+  function numMatchingIngredients(): number {
     return myIngredients.filter((x) =>
-				recipe.ingredients.map((i) => i.name.toLowerCase())
-				.includes(x["strIngredient"].toLowerCase()))
-			.length;
+      recipe.ingredients
+        .map((i) => i.name.toLowerCase())
+        .includes(x["strIngredient"].toLowerCase())
+    ).length;
   }
 
   function addMissingToShoppingList(ingredients: Ingredient[]) {
-    addToShoppingList(ingredients.map((x) => x.name)
-		      .filter((x) => !myIngredients.map((i) => 
-						       i["strIngredient"].toLowerCase())
-						       .includes(x.toLowerCase())))
+    addToShoppingList(
+      ingredients
+        .map((x) => x.name)
+        .filter(
+          (x) =>
+            !myIngredients
+              .map((i) => i["strIngredient"].toLowerCase())
+              .includes(x.toLowerCase())
+        )
+    );
   }
 
   return (
     <div className="card mb-4 text-start">
       <div className="row">
         <div className="col-md-4">
-	  <div className="container d-flex h-100 w-100 align-items-center">
-            <img className="img-fluid" src={ recipe.thumbnailUrl }/>
-	  </div>
+          <div className="container d-flex h-100 w-100 align-items-start mt-4">
+            <img className="img-fluid" src={recipe.thumbnailUrl} />
+          </div>
         </div>
         <div className="col-md-8">
           <div className="card-body">
-	    <button className="btn btn-light float-end" data-bs-toggle="dropdown">
-	      <i className="bi bi-three-dots-vertical"/>
-	    </button>
-	    <div className="dropdown-menu">
-	      <div className="dropdown-item-text btn btn-light"
-	           onClick={() => addMissingToShoppingList(recipe.ingredients)}>
-	        Add missing ingredients to shopping list
-	      </div>
-	    </div>
-            <h4 className="card-title">{ recipe.name }</h4>
-	    <h6 className="card-text">{ recipe.category }</h6>
-	    <h6 className="card-text">{ recipe.region }</h6>
-	    <p className="card-text">{ recipe.ingredients.map((x) => x.name).join(", ") }</p>
-	    <p className="card-text">
-	      You have { numMatchingIngredients() } out
-	      of { recipe.ingredients.length } ingredients
-	    </p>
+            <button
+              className="btn btn-light float-end"
+              data-bs-toggle="dropdown"
+            >
+              <i className="bi bi-three-dots-vertical" />
+            </button>
+            <div className="dropdown-menu">
+              <div
+                className="dropdown-item-text btn btn-light"
+                onClick={() => addMissingToShoppingList(recipe.ingredients)}
+              >
+                Add missing ingredients to shopping list
+              </div>
+              <div
+                hidden={showInstructions}
+                className="dropdown-item-text btn btn-light"
+                onClick={() => setShowInstructions(true)}
+              >
+                Show instructions
+              </div>
+              <div
+                hidden={!showInstructions}
+                className="dropdown-item-text btn btn-light"
+                onClick={() => setShowInstructions(false)}
+              >
+                Hide instructions
+              </div>
+            </div>
+            <div>
+              <h4 className="card-title">{recipe.name}</h4>
+              <h6 className="card-text">{recipe.category}</h6>
+              <h6 className="card-text">{recipe.region}</h6>
+              <p className="card-text">
+                {recipe.ingredients.map((x) => x.name).join(", ")}
+              </p>
+              <p className="card-text">
+                You have {numMatchingIngredients()} out of{" "}
+                {recipe.ingredients.length} ingredients
+              </p>
+              <p className="card-text" hidden={!showInstructions}>
+                {recipe.instructions}
+              </p>
+            </div>
           </div>
-	</div>
+        </div>
       </div>
     </div>
   );
--- app/recipesearch/components/recipe_list.tsx
+++ app/recipesearch/components/recipe_list.tsx
@@ -1,18 +1,23 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
-import { Recipe, jsonToRecipe } from './recipe';
-import { RecipeCard } from './recipe_card';
+import { useState, useEffect } from "react";
+import { Recipe, jsonToRecipe } from "./recipe";
+import { RecipeCard } from "./recipe_card";
 
 const resultsPerPage: number = 10;
 
-function RecipeList({ myIngredients, addToShoppingList }:
-		    { myIngredients: JSON[], addToShoppingList: (items: string[]) => void }) {
+function RecipeList({
+  myIngredients,
+  addToShoppingList,
+}: {
+  myIngredients: JSON[];
+  addToShoppingList: (items: string[]) => void;
+}) {
   const [searchText, setSearchText] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [pageNumber, setPageNumber] = useState<number>(0);
 
-  const mealSearchUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+  const mealSearchUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
   async function searchRecipes() {
     const response = await fetch(mealSearchUrl + searchText);
@@ -36,39 +41,71 @@ function RecipeList({ myIngredients, addToShoppingList }:
     <div className="card p-4">
       <div className="input-group">
         <i className="bi bi-search input-group-text" />
-        <input type="text" className="form-control" placeholder="Search for recipe name"
-               aria-label="Search for recipe by name"
-	       onChange={(e) => setSearchText(e.target.value)}></input>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for recipe name"
+          aria-label="Search for recipe by name"
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
       </div>
       <ul className="pagination mt-2">
-        <li className={`page-item ${pageNumber == 0 ? "disabled": ""}`}>
-          <a className="page-link" href="#" aria-label="Previous"
-	     onClick={() => setPageNumber(pageNumber - 1)}>
-            <i className="bi bi-chevron-left"/>
+        <li className={`page-item ${pageNumber == 0 ? "disabled" : ""}`}>
+          <a
+            className="page-link"
+            href="#"
+            aria-label="Previous"
+            onClick={() => setPageNumber(pageNumber - 1)}
+          >
+            <i className="bi bi-chevron-left" />
           </a>
         </li>
-        {[...new Array(Math.ceil(recipes.length / resultsPerPage)).keys()].map((page) =>
-          <li className={`page-item ${pageNumber == page ? "active": ""}`} key={page}>
- 	    <a className="page-link" href="#" aria-label={(page+1).toString()} 
-	       onClick={() => setPageNumber(page)}>
-	       {page+1}
-	    </a>
-	  </li>
+        {[...new Array(Math.ceil(recipes.length / resultsPerPage)).keys()].map(
+          (page) => (
+            <li
+              className={`page-item ${pageNumber == page ? "active" : ""}`}
+              key={page}
+            >
+              <a
+                className="page-link"
+                href="#"
+                aria-label={(page + 1).toString()}
+                onClick={() => setPageNumber(page)}
+              >
+                {page + 1}
+              </a>
+            </li>
+          )
         )}
-        <li className={`page-item
-	        ${(pageNumber + 1) * resultsPerPage > recipes.length ? "disabled": ""}`}>
-	  <a className="page-link" href="#" aria-label="Previous"
-	      onClick={() => setPageNumber(pageNumber + 1)}>
-	    <i className="bi bi-chevron-right"/>
- 	  </a>
+        <li
+          className={`page-item
+	        ${
+            (pageNumber + 1) * resultsPerPage > recipes.length ? "disabled" : ""
+          }`}
+        >
+          <a
+            className="page-link"
+            href="#"
+            aria-label="Previous"
+            onClick={() => setPageNumber(pageNumber + 1)}
+          >
+            <i className="bi bi-chevron-right" />
+          </a>
         </li>
       </ul>
-      {recipes.slice(pageNumber*resultsPerPage,
-		     pageNumber*resultsPerPage + resultsPerPage
-		    ).map((recipe) =>
-        <RecipeCard key={recipe.id} recipe={recipe} myIngredients={myIngredients}
-		    addToShoppingList={addToShoppingList}/>
-      )}
+      {recipes
+        .slice(
+          pageNumber * resultsPerPage,
+          pageNumber * resultsPerPage + resultsPerPage
+        )
+        .map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            recipe={recipe}
+            myIngredients={myIngredients}
+            addToShoppingList={addToShoppingList}
+          />
+        ))}
     </div>
   );
 }
